7dde91205f6bfed5d5579e701fbfd3ba9f597048
cemkiy <cemekiy@gmail.com>
2018-02-28 11:50:21 +0300

    types connect with mongo

--- src/types/torrent.js
+++ src/types/torrent.js
@@ -1,3 +1,6 @@
+// Mongoose schemas
+const UserModel = require('./models/user');
+
 let {
   GraphQLString,
   GraphQLInt,
@@ -6,6 +9,24 @@ let {
   GraphQLNonNull,
   GraphQLSchema
 } = require('graphql');
+
+const CommentType = new GraphQLObjectType({
+          name: 'CommentType',
+          fields: () => ({
+            user: {
+              type: UserType,
+              resolve: function(comment) {
+                return  UserModel.findById(comment.user_id);
+              }
+            },
+            text:{ type: new GraphQLNonNull(GraphQLString)},
+            subcomments: { type: new graphql.GraphQLList(CommentType)},
+            created_at:{ type: new graphql.GraphQLList(GraphQLString)},
+            updated_at:{ type: new graphql.GraphQLList(GraphQLString)},
+            deleted_at:{ type: new graphql.GraphQLList(GraphQLString)}
+          })
+  });
+
 
 // TorrentType for graphql
 const TorrentType = new GraphQLObjectType({
@@ -17,7 +38,7 @@ const TorrentType = new GraphQLObjectType({
     user: {
       type: UserType,
       resolve: function(torrent) {
-        return  torrent.user_id;
+        return  UserModel.findById(torrent.user_id);
       }
     },
     description:{type: new GraphQLNonNull(GraphQLString)},
@@ -33,28 +54,7 @@ const TorrentType = new GraphQLObjectType({
         })
     },
     screens:{ type: new graphql.GraphQLList(GraphQLString)},
-    comments:{ type: new GraphQLObjectType({
-            name: 'CommentType',
-            fields: () => ({
-              user: {
-                type: UserType,
-                resolve: function(torrent) {
-                  return  torrent.user_id;
-                }
-              },
-              text:{ type: new GraphQLNonNull(GraphQLString)},
-              subcomment: {
-                type: CommentType,
-                resolve: function(comment) {
-                  return  comment.subcomment;
-                }
-              }
-              created_at:{ type: new graphql.GraphQLList(GraphQLString)},
-              updated_at:{ type: new graphql.GraphQLList(GraphQLString)},
-              deleted_at:{ type: new graphql.GraphQLList(GraphQLString)}
-            })
-        })
-    },
+    comments:{ type: new graphql.GraphQLList(CommentType)},
     tags:{ type: new GraphQLObjectType({
             name: 'TagType',
             fields: () => ({
--- src/types/user.js
+++ src/types/user.js
@@ -1,3 +1,6 @@
+// Mongoose schemas
+const TorrentModel = require('./models/torrent');
+
 let {
   GraphQLString,
   GraphQLInt,
@@ -7,28 +10,31 @@ let {
   GraphQLSchema
 } = require('graphql');
 
+const KafaType = { type: new GraphQLObjectType({
+        name: 'KafaType',
+        description: "This represent a kafa",
+        fields: () => ({
+          torrent: {
+            type: TorrentType,
+            resolve: function(kafa) {
+              return  TorrentModel.findById(kafa.torrent_id);
+            }
+          },
+          kafa_count:{type: new GraphQLNonNull(GraphQLInt)}
+        })
+    })
+}
+
 // UserType for graphql
 const UserType = new GraphQLObjectType({
-  name: "User",
+  name: "UserType",
   description: "This represent an user",
   fields: () => ({
     id: {type: new GraphQLNonNull(GraphQLString)},
     username: {type: new GraphQLNonNull(GraphQLString)},
     email: {type: new GraphQLNonNull(GraphQLString)},
     birthday: {type: new GraphQLNonNull(GraphQLString)},
-    kafas: status: { type: new GraphQLObjectType({
-            name: 'KafaType',
-            fields: () => ({
-              torrent: {
-                type: TorrentType,
-                resolve: function(kafa) {
-                  return  kafa.torrent;
-                }
-              },
-              kafa_count:{type: new GraphQLNonNull(GraphQLInt)}
-            })
-        })
-    },
+    kafas: { type: new GraphQLList(KafaType)},
     created_at: {type: new GraphQLNonNull(GraphQLString)},
     updated_at: {type: new GraphQLNonNull(GraphQLString)},
     deleted_at: {type: new GraphQLNonNull(GraphQLString)}
