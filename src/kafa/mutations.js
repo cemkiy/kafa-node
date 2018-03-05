a2975f4489d3c5d2fc20d93a1c47eb15473feb3d
cemkiy <cemekiy@gmail.com>
2018-03-05 15:13:08 +0300

    kafa works

--- src/kafa/mutations.js
+++ src/kafa/mutations.js
@@ -1,3 +1,5 @@
+
+
 // Mongoose schemas
 const kafaModel = require('./models.js');
 
@@ -5,12 +7,12 @@ const kafaModel = require('./models.js');
 const kafaTypes = require('./types.js');
 
 let {
-  GraphQLString,
-  GraphQLInt,
-  GraphQLList,
-  GraphQLObjectType,
-  GraphQLNonNull,
-  GraphQLSchema
+	GraphQLString,
+	GraphQLInt,
+	GraphQLList,
+	GraphQLObjectType,
+	GraphQLNonNull,
+	GraphQLSchema
 } = require('graphql');
 
 // mutation {
@@ -24,71 +26,62 @@ let {
 
 // This is the Root Mutation
 const KafaMutationRootType = module.exports = new GraphQLObjectType({
-  name: 'KafaMutationAppSchema',
-  description: "Kafa Schema Mutation Root",
-  fields: () => ({
-    createKafa: {
-    type: new GraphQLNonNull(kafaTypes.KafaType),
-    args: {
-      input: {
-        type: new GraphQLNonNull(kafaTypes.KafaInputType),
-      },
-    },
-    resolve: async (rootValue, { input }) => {
-      const result = await new Promise((resolve) => {
-        setTimeout(() =>
-          resolve(
-            kafaModel.create(input, (err, kafa) => {
-              if(err) throw err;
-              return kafa;
-            })
-          ), 100);
-      });
-        return result;
-        }
-    },
-    updateKafa: {
-    type: new GraphQLNonNull(kafaTypes.KafaType),
-    args: {
-      id: {
-        type: new GraphQLNonNull(GraphQLString),
-      },
-      input: {
-        type: new GraphQLNonNull(kafaTypes.KafaInputType),
-      }
-    },
-    resolve: async (rootValue, args) => {
-      const result = await new Promise((resolve) => {
-        setTimeout(() =>
-          resolve(
-            kafaModel.update(args.id, args.input, (err, updatedKafa) => {
-              if(err) throw err;
-              return updatedKafa;
-            })
-          ), 100);
-      });
-        return result;
-        }
-    },
-    deleteKafa: {
-    type: GraphQLString,
-    args: {
-      id: {
-        type: new GraphQLNonNull(GraphQLString),
-      }
-    },
-    resolve: async (rootValue, args) => {
-      const result = await new Promise((resolve) => {
-        setTimeout(() =>
-          resolve(
-            kafaModel.findByIdAndRemove(args.id, (err, kafa) => {
-              if (err) return "failure";
-              return "deleted"
-          })
-          ), 100);
-      });
-        return result;
-        }
-    }
-  })
+	name: 'KafaMutationAppSchema',
+	description: "Kafa Schema Mutation Root",
+	fields: () => ({
+		createKafa: {
+			type: new GraphQLNonNull(kafaTypes.KafaType),
+			args: {
+				input: {
+					type: new GraphQLNonNull(kafaTypes.KafaCreateInputType),
+				},
+			},
+			resolve: function (parent, {
+				input
+			}, ast) {
+				return kafaModel.create(input).then(function (kafa) {
+					return kafa
+				})
+			}
+		},
+		updateKafa: {
+			type: new GraphQLNonNull(kafaTypes.KafaType),
+			args: {
+				id: {
+					type: new GraphQLNonNull(GraphQLString),
+				},
+				input: {
+					type: new GraphQLNonNull(kafaTypes.KafaUpdateInputType),
+				}
+			},
+			resolve: function (parent, args, ast) {
+				return kafaModel.findByIdAndUpdate(args.id, {
+						"$set": args.input
+					}).exec()
+					.then((kafa) => {
+						return kafa
+					})
+					.catch((err) => {
+						throw err;
+					})
+			}
+		},
+		deleteKafa: {
+			type: GraphQLString,
+			args: {
+				id: {
+					type: new GraphQLNonNull(GraphQLString),
+				}
+			},
+			resolve: function (parent, args, ast) {
+				return kafaModel.findByIdAndRemove(args.id).exec()
+					.then(() => {
+						return "deleted"
+					})
+					.catch((err) => {
+						throw err;
+					})
+			}
+		}
+	})
 });
--- src/kafa/queries.js
+++ src/kafa/queries.js
@@ -1,3 +1,5 @@
+
+
 // Mongoose schemas
 const torrentModel = require('../torrent/models.js');
 const userModel = require('./users/models.js');
@@ -9,85 +11,89 @@ const userTypes = require('./user/types.js');
 const kafaTypes = require('./types.js');
 
 let {
-  GraphQLString,
-  GraphQLInt,
-  GraphQLList,
-  GraphQLObjectType,
-  GraphQLNonNull,
-  GraphQLSchema
+	GraphQLString,
+	GraphQLInt,
+	GraphQLList,
+	GraphQLObjectType,
+	GraphQLNonNull,
+	GraphQLSchema
 } = require('graphql');
 
 
 // This is the Root Query
 const KafaQueryRootType = module.exports = new GraphQLObjectType({
-  name: 'KafaQuerySchema',
-  description: "Kafa Schema Query Root",
-  fields: () => ({
-    kafas: {
-      type: new GraphQLList(kafaTypes.KafaType),
-      description: "List of all Kafas",
-      args: {
-        user_id: {
-          name: 'user_id',
-          type: new GraphQLNonNull(GraphQLString)
-        },
-        torrent_id: {
-          name: 'torrent_id',
-          type: new GraphQLNonNull(GraphQLString)
-        },
-        created_at_from: {
-          name: 'created_at_from',
-          type: new GraphQLNonNull(GraphQLString)
-        },
-        created_at_to: {
-          name: 'created_at_to',
-          type: new GraphQLNonNull(GraphQLString)
-        },
-        updated_at_from: {
-          name: 'updated_at_from',
-          type: new GraphQLNonNull(GraphQLString)
-        },
-        updated_at_to: {
-          name: 'updated_at_to',
-          type: new GraphQLNonNull(GraphQLString)
-        },
-        limit: {
-          name: 'limit',
-          type: new GraphQLNonNull(GraphQLInt)
-        },
-        skip: {
-          name: 'skip',
-          type: new GraphQLNonNull(GraphQLInt)
-        },
-        sort_field: {
-          name: 'sort_field',
-          type: new GraphQLNonNull(GraphQLString)
-        },
-        sort_type: {
-          name: 'sort_type',
-          type: new GraphQLNonNull(GraphQLInt)
-        }
-      },
-      resolve: function(parent, args, ast) {
-        kafaModel.list(args, (err, kafas) => {
-          if (err)
-            throw err;
-          return kafas;
-        });
-      }
-    },
-    kafaById: {
-      type: new GraphQLNonNull(kafaTypes.KafaType),
-      description: "Get kafa by id",
-      args: {
-        id: {
-          name: 'id',
-          type: new GraphQLNonNull(GraphQLString)
-        }
-      },
-      resolve: function(parent, args, ast) {
-        return kafaModel.findById(args.id);
-      }
-    }
-  })
+	name: 'KafaQuerySchema',
+	description: "Kafa Schema Query Root",
+	fields: () => ({
+		kafas: {
+			type: new GraphQLList(kafaTypes.KafaType),
+			description: "List of all Kafas",
+			args: {
+				user_id: {
+					name: 'user_id',
+					type: GraphQLString
+				},
+				torrent_id: {
+					name: 'torrent_id',
+					type: GraphQLString
+				},
+				created_at_from: {
+					name: 'created_at_from',
+					type: GraphQLString
+				},
+				created_at_to: {
+					name: 'created_at_to',
+					type: GraphQLString
+				},
+				updated_at_from: {
+					name: 'updated_at_from',
+					type: GraphQLString
+				},
+				updated_at_to: {
+					name: 'updated_at_to',
+					type: GraphQLString
+				},
+				limit: {
+					name: 'limit',
+					type: GraphQLInt
+				},
+				skip: {
+					name: 'skip',
+					type: GraphQLInt
+				},
+				sort_field: {
+					name: 'sort_field',
+					type: GraphQLString
+				},
+				sort_type: {
+					name: 'sort_type',
+					type: GraphQLInt
+				}
+			},
+			resolve: function (parent, args, ast) {
+				return kafaModel.list(args, (err, kafas) => {
+					if (err)
+						throw err;
+					return kafas;
+				});
+			}
+		},
+		kafaById: {
+			type: new GraphQLNonNull(kafaTypes.KafaType),
+			description: "Get kafa by id",
+			args: {
+				id: {
+					name: 'id',
+					type: new GraphQLNonNull(GraphQLString)
+				}
+			},
+			resolve: function (parent, args, ast) {
+				return kafaModel.getById(args.id, (err, kafa) => {
+					if (err)
+						throw err;
+					return kafa;
+				});
+			}
+		}
+	})
 });
